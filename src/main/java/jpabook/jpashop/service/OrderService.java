5736428013c7926ef03dfbaa028bb634c617289b
Jiwon <wldnjs980409@naver.com>
2023-08-29 10:16:34 +0900

    주문 목록 검색

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -53,7 +54,9 @@ public class OrderService {
      */
     @Transactional
     public void cancelOrder(Long orderId){
+        // 주문 엔터티 조회
         Order order = orderRepository.findOne(orderId);
+        // 주문 취소
         order.cancel();
     }
 
@@ -61,7 +64,7 @@ public class OrderService {
     /**
      * 검색
      */
-//    public List<Order> findOrders(OrderSearch orderSearch){
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
